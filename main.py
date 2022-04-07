6b4738eec7f2761f9a4e9df9c1c6ebe391214d7a
Christina Lee <christinlee367@gmail.com>
2022-04-07 12:01:25 -0700

    base files

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-# import "packages" from flask
-
 from flask import Flask, render_template, request
-import requests
 
-# create a Flask instance
+# create an instance of flask object
 app = Flask(__name__)
 
-# connects default URL to render index.html
 
-@app.route("/",methods=['GET', 'POST'])
+# home page accessed with http://127.0.0.1:5000/
+@app.route("/")
 # map URL route for function below
-def base():
-    return render_template("layouts/base.html")
+def index():
+    return render_template("index.html")
 
 
-# runs the application on the development server
+@app.route("/about/")
+def about():
+    return render_template("layouts/about.html")
+
+
+
+# from image import hide_msg
+# @app.route("/rgbhide")
+# def hidemsg():
+#    hide_msg()
+
 if __name__ == "__main__":
-    app.run(debug=True, port="5001")
+    app.run(debug=True)
